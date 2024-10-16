641bc00f811708caec316c45f80cb110190ece40
Raul Silveira <raulantsilveira@gmail.com>
2024-10-15 23:08:20 -0300

    setting the basic routes configs

---
+++ src/database.js
@@ -0,0 +1,79 @@
+import fs from 'node:fs/promises'
+
+const databasePath = new URL('../db.json', import.meta.url)
+// usamos essa classe URL nativa do js para determinar
+// o caminho que é para o writeFile criar o db.json pois
+// se nao ele sempre criara com base na pasta que estamos 
+// aberto no terminal. ex: se eu nao tenho o db.json criado,
+// e no terminal to na pasta streams, o db.json sera criado la,
+// e isso nao é pra acontecer, quero que seja sempre na raiz do meu
+// projeto
+
+export class Database {
+    #database = {}
+    // esse # faz com que database seja uma prop privada,
+    // entao nao conseguimos acessar ele diretamente
+    // isso é otimo quando se trata de db
+
+
+    constructor () {
+        fs.readFile(databasePath, 'utf8')
+        .then(data =>{
+            this.#database = JSON.parse(data)
+        })
+        .catch(() => {
+            this.#persist()
+        })
+    }
+
+
+
+    #persist () {
+        fs.writeFile(databasePath, JSON.stringify(this.#database))
+    }
+
+    select(table, search) {
+        let data = this.#database[table] ?? []
+    
+        if (search) {
+            data = data.filter(row => {
+                return Object.entries(search).some(([key, value]) => {
+                    return row[key].toLowerCase().includes(value.toLowerCase())
+                })
+            })
+        }
+    
+        return data
+    }
+    
+
+    insert(table, data) {
+        if (Array.isArray(this.#database[table])) {
+            this.#database[table].push(data)
+    } else {
+        this.#database[table] = [data]
+    }
+
+    this.#persist();
+
+    return data;
+    }
+
+    delete(table, id) {
+        const rowIndex = this.#database[table].findIndex(row => row.id == id)
+        
+        if (rowIndex > -1) {
+            this.#database[table].splice(rowIndex, 1)
+            this.#persist()
+        }
+    }
+
+    update(table, id, data) {
+        const rowIndex = this.#database[table].findIndex(row => row.id == id)
+        
+        if (rowIndex > -1) {
+            this.#database[table][rowIndex] = { id, ...data }
+            this.#persist()
+        }
+    }
+}
---
+++ src/server.js
@@ -0,0 +1,42 @@
+import http from 'node:http'
+import { json } from './middlewares/json.js'
+import { routes } from './routes.js'
+import { extractQueryParams } from './utils/extract-query-params.js'
+
+
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req
+
+  await json(req, res)
+
+  const route = routes.find(route => {
+    // esse "test" é uma funçao que as regex tem
+    // ele retorna booleano. neste caso, se a url tiver certo,
+    // retornara true. caso contrario, false.
+    
+    
+    // console.log(route.path.test(url))
+    return route.method == method && route.path.test(url)
+    
+  })
+
+  if (route) {
+
+    const routeParams = req.url.match(route.path)
+    
+    // console.log(extractQueryParams(routeParams.groups.query))
+
+    const { query, ...params } = routeParams.groups
+    
+    req.params = params 
+    req.query = query ? extractQueryParams(query) : {}
+    //é usado o { ... } para tirar aquele "Object null prototype"
+    // que fica aparecendo junto quando printamos os groups
+
+    return route.handler(req, res)
+  }
+
+  return res.writeHead(404).end('Not found')
+})
+
+server.listen(3335)
